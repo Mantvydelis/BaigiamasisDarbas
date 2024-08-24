bb853fe8e01e02879a19ef5702c45f8ae770f3f6
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-08-24 17:43:25 +0300

    TestProduktai

---
+++ ProduktaiTest/TestProduktai.cs
@@ -0,0 +1,67 @@
+using Parduotuve.Core.Models;
+
+namespace ProduktaiTest
+{
+    public class TestProduktai
+    {
+        [Fact]
+        public void TestAddProduct()
+        {
+
+
+
+
+        }
+
+        [Fact]
+        public void TestUpdateProduct()
+        {
+
+
+
+
+        }
+
+
+        [Fact]
+        public void TestGetAllProducts()
+        {
+
+
+
+
+        }
+
+        [Fact]
+        public void TestGetProductById()
+        {
+
+
+
+
+        }
+
+
+        [Fact]
+        public void TestDeleteProductById()
+        {
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
