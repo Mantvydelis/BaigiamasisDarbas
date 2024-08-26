fc7ad23e2fef4354b63a0c4c898b0033ea8782a0
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-08-26 13:27:05 +0300

    ProduktaiController

--- Parduotuve.API/Controllers/ProductaiController.cs
+++ Parduotuve.API/Controllers/ProductaiController.cs
@@ -1,12 +1,88 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Parduotuve.Core.Contracts;
+using Parduotuve.Core.Models;
 
 namespace Parduotuve.API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ProductaiController : Controller
     {
-        public IActionResult Index()
+        private readonly IProduktaiService _produktaiService;
+
+        public ProductaiController(IProduktaiService produktaiService)
         {
-            return View();
+            _produktaiService = produktaiService;
         }
+
+
+        [HttpGet("GetAllProducts")]
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _produktaiService.GetAllProducts();
+            return Ok(allProducts);
+        }
+
+
+        [HttpPost("AddProduct")]
+        public async Task<IActionResult> AddProduct(Produktas produktas)
+        {
+            try
+            {
+                await _produktaiService.AddProduct(produktas);
+                return Ok();
+            }
+            catch
+            {
+                return Problem();
+            }
+
+        }
+
+
+
+        [HttpGet("GetProductById")]
+        public async Task<IActionResult> GetProductById(int produktoId)
+        {
+            try
+            {
+                var foundProduct = await _produktaiService.GetProductById(produktoId);
+                return Ok(foundProduct);
+
+            }
+            catch
+            {
+
+                return Problem();
+
+            }
+
+        }
+
+        [HttpPatch("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct(Produktas produktas)
+        {
+            try
+            {
+                await _produktaiService.UpdateProduct(produktas);
+                return Ok(produktas);
+
+            }
+            catch
+            {
+                return Problem();
+            }
+
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public async Task<IActionResult> DeleteProductById(int produktoId)
+        {
+            await _produktaiService.DeleteProductById(produktoId);
+            return Ok();
+        }
+
+
+
     }
 }
